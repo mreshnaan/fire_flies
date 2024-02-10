ed641e6ea48126d2c83b9e0d8bbde845a7484e0b
unknown <mreshnaan7@gmail.com>
2024-02-10 08:32:42 +0530

    mobile responsive 2

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -8,14 +8,14 @@ function TeamSection() {
             <div className='flex flex-col gap-[60px]'>
                 <div className='flex gap-[12px] xl:gap-[30px] items-center justify-center'>
                     <div className='border-[#70757E] lg:hidden w-full border-opacity-30 border-t h-[1px]'></div>
-                    <h1 className='text-[#505D65] w-fit text-[20px] leading-[20px] xl:w-[330px] xl:text-[48px] font-bold xl:leading-[48px]'>Physicist to Physicist</h1>
+                    <h1 className='text-[#505D65] w-fit text-[20px] leading-[20px] xl:w-[330px] xl:text-[48px] font-bold xl:leading-[48px]'>President-CEO </h1>
                     <div className='border-[rgb(112,117,126)] w-full xl:w-[800px] 2xl:w-[1079px] border-opacity-30 border-t h-[1px]'></div>
                 </div>
                 <div className="flex flex-col items-center xl:flex-row xl:gap-[200px] xl:pl-[50px]">
                     <img src="/assets/president.png" alt="" className="w-[250px] h-[220.93px] xl:w-[516px] xl:h-[491px]" />
                     <div className="flex flex-col items-center gap-[12px] xl:gap-[30px]">
                         <h1 className="text-[24px] leading-[26.4px] xl:text-[48px] font-bold xl:leading-[72px] text-[#505D65]">Mr. Ulrich Märki</h1>
-                        <h2 className="text-[12px] leading-[18px] xl:text-[24px] xl:leading-[36px] text-[#505D65] font-bold">Physicist to Physicist, passionate traveler</h2>
+                        <h2 className="text-[12px] leading-[18px] xl:text-[24px] xl:leading-[36px] text-[#505D65] font-bold">President-CEO , passionate traveler</h2>
                         <div className="flex flex-col gap-[8px] w-[300px] xl:gap-[16px] xl:w-full">
                             <div className="flex gap-5 items-center">
                                 <svg className="w-[14px] h-[14px] xl:w-[20px] xl:h-[20px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none">
@@ -27,7 +27,7 @@ function TeamSection() {
                                 <svg className="w-[14px] h-[14px] xl:w-[20px] xl:h-[20px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none">
                                     <circle cx="10" cy="10" r="9" stroke="#13AFB6" stroke-width="2" />
                                 </svg>
-                                <p className="text-[#505D65] text-[12px] leading-[24px] xl:text-[15px] xl:leading-[25px]">physicist-mathematician</p>
+                                <p className="text-[#505D65] text-[12px] leading-[24px] xl:text-[15px] xl:leading-[25px]">Physicist-mathematician</p>
                             </div>
                             <div className="flex gap-5 items-center">
                                 <svg className="w-[14px] h-[14px] xl:w-[20px] xl:h-[20px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none">
@@ -61,14 +61,14 @@ function TeamSection() {
                 <div className="hidden xl:grid grid-cols-1 self-center xl:grid-cols-3 2xl:grid-cols-5 gap-[80px]">
 
                     <TeamPerson img="/assets/team1.png" name="Mr. Ulrich Märki" description="Chairman of the Board Swizerland" />
-                    <TeamPerson img="/assets/team2.png" name="Hungary to Dubai" description="Director of Business Development Hungary" />
+                    <TeamPerson img="/assets/team2.png" name="István Varga" description="Director of Business Development Hungary to Dubai" />
                     <TeamPerson img="/assets/team3.png" name="Zoltán Hidas" description="CFO USA" />
                     <TeamPerson img="/assets/team4.png" name="Anatol Pante" description="Chief Accountant Switzerland" />
                     <TeamPerson img="/assets/team5.png" name="Silver Borer" description="Data Protection Supervisor, Lawyer Switzerland" />
                 </div>
                 <CustomCarousel sliders={[
                     <TeamPerson img="/assets/team1.png" name="Mr. Ulrich Märki" description="Chairman of the Board Swizerland" />,
-                    <TeamPerson img="/assets/team2.png" name="Hungary to Dubai" description="Director of Business Development Hungary" />,
+                    <TeamPerson img="/assets/team2.png" name="István Varga" description="Director of Business Development Hungary to Dubai" />,
                     <TeamPerson img="/assets/team3.png" name="Zoltán Hidas" description="CFO USA" />,
                     <TeamPerson img="/assets/team4.png" name="Anatol Pante" description="Chief Accountant Switzerland" />,
                     <TeamPerson img="/assets/team5.png" name="Silver Borer" description="Data Protection Supervisor, Lawyer Switzerland" />,
